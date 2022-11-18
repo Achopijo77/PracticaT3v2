9e8549c287d01fd5e14abd1a45ebd0ee187f09a8
APS <silcros@gmail.com>
2022-11-18 11:53:23 +0100

    Se ha modificado el código para que funciones correcteamente

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -21,30 +21,27 @@ namespace WindowsFormsApp1
         {
             string textoTelegrama;
             char tipoTelegrama = ' ';
-            int numPalabras = 0;
             double coste;
-            //Leo el telegrama
+
             textoTelegrama = txtTelegrama.Text;
-            // telegrama urgente?
+            String[] palabras = textoTelegrama.Split(' ');
+            int numPalabras = palabras.Length;
+
             if (cbUrgente.Checked)
                 tipoTelegrama = 'u';
-            //Obtengo el número de palabras que forma el telegrama
-            numPalabras = textoTelegrama.Length;
-            //Si el telegrama es ordinario
-            if (tipoTelegrama == 'o')
-                if (numPalabras <= 10)
-                    coste = 25;
-                else
-                    coste = 0.5*numPalabras;
-            else
-            //Si el telegrama es urgente
+
             if (tipoTelegrama == 'u')
                 if (numPalabras <= 10)
                     coste = 5;
                 else
                     coste = 5 + 0.75 * (numPalabras - 10);
             else
-                coste = 0;
+                if (numPalabras <= 10)
+                coste = 2.5;
+            else
+                coste = 2.5 + 0.5 * (numPalabras - 10);
+
+
             txtPrecio.Text = coste.ToString() + " euros";
         }
     }
